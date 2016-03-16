fae77f4813c63b837a4a7abd36d51889c5b313f3
Jeffrey Knight <Jeffrey.757knight@gmail.com>
2016-03-15 21:52:19 -0400

    Commit 4
    This contains SwapName

---
+++ components/buildingCodeRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Knight_Jeffrey_BuildingCodeRepository
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			SwapName ();
+		}
+
+		//exercise1//
+		public static void SwapName()
+		{
+			//varaibles
+			string firstName = "";
+			string lastName = "";
+			string temp = "";
+
+			Console.WriteLine ("Enter your first name.");
+			firstName = Console.ReadLine ();
+			Console.WriteLine ("Enter your last name.");
+			lastName = Console.ReadLine ();
+			Console.WriteLine (firstName+" "+lastName);
+
+			temp = firstName;
+			firstName = lastName;
+			lastName = temp;
+			Console.WriteLine (firstName+" "+lastName);
+		}
+		//exercise1//
+
+
+	}
+}
