26ddfd868704d70b801563cb8413907b7e1fd176
Jeffrey Knight <Jeffrey.757knight@gmail.com>
2017-08-09 19:27:25 -0400

    PPAssignment1 commit 2
    This codes adds additional functionality which allows for the user to
    move items from one list and list view to the other and remove items
    from the lists and listviews.

--- components/groceryList.cs
+++ components/groceryList.cs
@@ -40,24 +40,85 @@ namespace KnightJeffrey_groceryList_assignment1
             listView2.Items.Add(lvi);              //new listview item is added to the the listview
 
         }
-        /*
-        private void updateNeeds()
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnRemove.Visible = true;
+            btnSwitch.Visible = true;
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnRemove.Visible = true;
+            btnSwitch.Visible = true;
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
         {
             ListViewItem lvi = new ListViewItem();
-            foreach (string item in needs)
+
+            if (listView1.SelectedItems.Count >0) //determines if selected item is from listview1
             {
-                lvi.Text = item;
-                listView1.Items.Add(lvi);
+                lvi.Text = listView1.SelectedItems[0].Text; //sets the new listview item text to the text of the selected listviewitem
+                listView1.Items.RemoveAt(listView1.SelectedIndices[0]); //removes selected item from the list view
+                listView2.Items.Add(lvi); //adds the selected item to the other list view
+                wants.Add(lvi.Text); //adds selected item to other list
+
+                for (int i = 0; i < needs.Count; i++) //finds selected item in the current list and removes it
+                {
+                    if (needs[i] == lvi.Text)
+                    {
+                        needs.RemoveAt(i);
+                    }
+                }
+            }
+            if (listView2.SelectedItems.Count >0) //determines if selected item is from listview2
+            {
+                lvi.Text = listView2.SelectedItems[0].Text; //sets the new listview item text to the text of the selected listviewitem
+                listView2.Items.RemoveAt(listView2.SelectedIndices[0]); //removes selected item from the list view
+                listView1.Items.Add(lvi); //adds the selected item to the other list view
+                needs.Add(lvi.Text); //adds selected item to other list
+
+                for (int i = 0; i < wants.Count; i++) //finds selected item in the current list and removes it
+                {
+                    if (wants[i] == lvi.Text)
+                    {
+                        wants.RemoveAt(i);
+                    }
+                }
             }
         }
-        private void updateWants()
+
+        private void btnRemove_Click(object sender, EventArgs e)
         {
-            ListViewItem lvi = new ListViewItem();
-            foreach (string item in wants)
+            string item = "";
+            if (listView1.SelectedItems.Count > 0) //determines if selected item is from listview1
             {
-                lvi.Text = item;
-                listView2.Items.Add(lvi);
+                item = listView1.SelectedItems[0].Text;
+                listView1.Items.RemoveAt(listView1.SelectedIndices[0]);
+                for (int i = 0; i < needs.Count; i++) //finds selected item in the current list and removes it
+                {
+                    if (needs[i] == item)
+                    {
+                        needs.RemoveAt(i);
+                    }
+                }
+
             }
-        }*/
+
+            if (listView2.SelectedItems.Count > 0) //determines if selected item is from listview1
+            {
+                item = listView2.SelectedItems[0].Text;
+                listView2.Items.RemoveAt(listView2.SelectedIndices[0]);
+                for (int i = 0; i < wants.Count; i++) //finds selected item in the current list and removes it
+                {
+                    if (wants[i] == item)
+                    {
+                        wants.RemoveAt(i);
+                    }
+                }
+            }
+
+        }
     }
 }
