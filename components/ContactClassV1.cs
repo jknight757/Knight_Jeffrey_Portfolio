353ffa081806cdb35c00a8df5d48e578d7c56291
Jeffrey Knight <Jeffrey.757knight@gmail.com>
2017-08-13 13:57:48 -0400

    ContactListAssignment2 commit 1
    This commit provides the basic functionality of the contact
    application, there is a contact class that allows for contacts to be
    instantiated and hold proper values. The user can create a new contact
    and add it to the list view.

---
+++ components/ContactClassV1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightJeffreyContactList
+{
+    //class to hold contact info
+    class Contact
+    {
+        string first;
+        string last;
+        long phone;
+        string email;
+
+        //constructor
+        public Contact(string f, string l,long p,string e)
+        {
+            first = f;
+            last = l;
+            phone = p;
+            email = e;
+        }
+        public override string ToString()
+        {
+            return first + " " + last;
+        }
+
+    }
+}
---
+++ components/ContactListForm1V1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnightJeffreyContactList
+{
+    public partial class Form1 : Form
+    {
+        List<Contact> contacts = new List<Contact>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e) //creates instance of contact class with user input and adds to list view and list
+        {
+            ListViewItem lvi = new ListViewItem();
+
+            long x=0;
+            if (long.TryParse(txtNump1.Text, out x)) //checks to make sure phone number is an long integer
+            {
+
+
+
+
+                Contact c1 = new Contact(txtFirstN.Text, txtLastN.Text, x, txtEmail.Text); //creates new contact
+                lvi.Tag = c1;
+                lvi.Text = c1.ToString();
+                //lvi.ImageIndex = 0;
+                listView1.Items.Add(lvi); //adds to listview
+                contacts.Add(c1);
+            }
+            
+        }
+        
+    }
+}
