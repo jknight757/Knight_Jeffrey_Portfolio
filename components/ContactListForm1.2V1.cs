809400352ecba1177121dd9a4db1f57c86db6e74
Jeffrey Knight <Jeffrey.757knight@gmail.com>
2017-09-07 19:36:35 -0400

    ContactList Commit 4
    This Commit provides functionality that allows the user to enter input
    into the input fields, when the user adds this information and clicks
    the button an instance of the contact class is created accepting input
    fields as the parameters, before a new contact is created all of the
    fields are checked for blank input. Once a new contact has been created
    a list view item is created and its values are set to the values of the
    contact.
    Text boxes are cleared and blank input message is shown if there are
    blank fields.

---
+++ components/ContactListForm1.2V1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+//Jeffrey Knight
+//Project and Portfolio 3
+// 9/7/17
+namespace KnightJeffreyContactList
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)               //creates instance of contact class with user input and adds to list view and list
+        {
+            //txtFirstN.Update();
+            ListViewItem lvi = new ListViewItem();
+            if (txtFirstN.Text != "" && txtLastN.Text != "" && txtEmail.Text != "" && txtNump1.Text != "")
+            {
+                long x = long.Parse(txtNump1.Text);
+
+                Contact c1 = new Contact(txtFirstN.Text, txtLastN.Text, x, txtEmail.Text); //creates new contact
+                lvi.Tag = c1;
+                lvi.Text = c1.ToString();
+                lvi.ImageIndex = 0;
+                listView1.Items.Add(lvi);                                                   //adds to listview
+                txtFirstN.Text = "";
+                txtLastN.Text = "";
+                txtEmail.Text = "";
+                txtNump1.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("All fields must have input");
+            }
+
+        }
+
+        
+    }
+}
