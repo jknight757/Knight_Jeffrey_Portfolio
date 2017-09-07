0b00c6d5c936966f9e464de052714c71b35c01f7
Jeffrey Knight <Jeffrey.757knight@gmail.com>
2017-09-07 19:27:38 -0400

    ContactList Commit 2
    This commit has the properties of the contact class member variables
    (The getters and setters for all member variables ).

---
+++ components/ContactClass2V2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightJeffreyContactList
+{
+    //class to hold contact info
+     class Contact
+    {
+        private string first;
+        private string last;
+        private long phone;
+        private string email;
+
+        //constructor
+        public Contact(string f, string l,long p,string e)
+        {
+            first = f;
+            last = l;
+            phone = p;
+            email = e;
+        }
+        public string First
+        {
+            get
+            {
+                return first;
+            }
+            set
+            {
+                first = value;
+            }
+        }
+        public string Last
+        {
+            get
+            {
+                return last;
+            }
+            set
+            {
+                last = value;
+            }
+        }
+        public long Phone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                phone = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        
+
+    }
+}
