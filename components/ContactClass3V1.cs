44d4368fc5e4f18bcf6dda904a78c1b993b276d4
Jeffrey Knight <Jeffrey.757knight@gmail.com>
2017-10-05 15:19:15 -0400

    ContactList3 Commit 1
    This commit creates the basis of the contact class. The contact class
    is created as well as member variables. A constructor is created as
    well to assign values to the member variables upon creation of a
    instance of the contact class.

---
+++ components/ContactClass3V1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightJeffreyContactList
+{
+    //class to hold contact info
+     class Contact
+    {
+        private string first;
+        private string last;
+        private long phone;
+        private string email;
+
+        //constructor
+        public Contact(string f, string l,long p,string e)
+        {
+            first = f;
+            last = l;
+            phone = p;
+            email = e;
+        }
+
+    }
+}
