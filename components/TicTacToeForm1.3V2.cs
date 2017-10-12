6d90f3f204640c18720b6241ec0ec70dc9cc8db0
Jeffrey Knight <Jeffrey.757knight@gmail.com>
2017-10-12 15:31:03 -0400

    TicTacToe3 Commit 2
    This commit provides functionality for the Red and Blue buttons. There
    are 2 methods for each button click. When either button is clicked the
    game pieces image lists are changed accordingly and a check mark is
    displayed next to the selected color.

---
+++ components/TicTacToeForm1.3V2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+//Jeffrey Knight
+//Project and portfolio 3 10/12/17
+//TicTacToe
+namespace TicTacToe
+{
+    public partial class frmTicTacToe : Form
+    {
+        int imgIndex=-1;                                              //will hold the image index which is seleted when the player chooses X or O
+        int numPlays = 0;                                             //counter which holds th number of squares that have been played
+        List<bool> tilesPlayed = new List<bool> { false, false, false, false, false, false, false, false, false }; //holds boolean value to store if a tile has already been played
+        string whichImageList = "blueImages";
+        int[,] playerTiles =new int[3,3] { { -1, -2, -3}              //2D array stores who played each tile       
+                                         , { -4, -5, -6}
+                                         , { -7, -8, -9 } };
+        
+
+        /* THINGS TO CONSIDER:
+            - You must change the project name to conform to the required
+              naming convention.
+            - You must comment the code throughout.  Failure to do so could result
+              in a lower grade.
+            - All button names and other provided variables and controls must
+              remain the same.  Changing these could result in a 0 on the project.
+            - Selecting Blue or Red on the View menu should change the imageList
+              attached to all buttons so that any current play will change the color
+              of all button images.
+            - Saved games should save to XML.  A game should load only from XML and
+              should not crash the application if a user tries to load an incorrect 
+              file.
+        */
+
+        public frmTicTacToe()
+        {
+            InitializeComponent();
+        }
+        
+        private void xToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            imgIndex = 1;
+        }
+
+        private void oToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            imgIndex = 0;
+        }
+
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e) //changes all buttons image list to redImages 
+        {
+
+            r1c1button.ImageList = redImages; 
+            r1c2button.ImageList = redImages;
+            r1c3button.ImageList = redImages;
+            r2c1button.ImageList = redImages;
+            r2c2button.ImageList = redImages;
+            r2c3button.ImageList = redImages;
+            r3c1button.ImageList = redImages;
+            r3c2button.ImageList = redImages;
+            r3c3button.ImageList = redImages;
+            whichImageList = "redImages";
+            redToolStripMenuItem.Checked = true;   //checks red option
+            blueToolStripMenuItem.Checked = false; //unchecks blue option
+        }
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e) //changes all buttons image list to blueImages
+        {
+            r1c1button.ImageList = blueImages;
+            r1c2button.ImageList = blueImages;
+            r1c3button.ImageList = blueImages;
+            r2c1button.ImageList = blueImages;
+            r2c2button.ImageList = blueImages;
+            r2c3button.ImageList = blueImages;
+            r3c1button.ImageList = blueImages;
+            r3c2button.ImageList = blueImages;
+            r3c3button.ImageList = blueImages;
+            whichImageList = "blueImages";
+            redToolStripMenuItem.Checked = false;  //unchecks red option
+            blueToolStripMenuItem.Checked = true;  //checks blue option
+        }
+    }
+}
